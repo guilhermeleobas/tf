5e6cf70ea6b493ac1f03228275b4451bb514110d
Guilherme <guilhermeleobas@gmail.com>
2018-03-29 22:22:38 -0300

    update info.sh

--- Benchs/Prolangs-C/agrep/info.sh
+++ Benchs/Prolangs-C/agrep/info.sh
@@ -6,4 +6,4 @@ exe_name="$bnc_name.exe" ;
 
 source_files=( "asearch.c" "asearch1.c" "bitap.c" "checkfile.c" "compat.c" "follow.c" "main.c" "maskgen.c" "mgrep.c" "parse.c" "preprocess.c" "sgrep.c" "utilities.c" )
 
-RUN_OPTIONS="-2 int InstructionCombining.cpp.txt" ;
+RUN_OPTIONS="-2 int InstructionCombining.cpp" ;
