d1ea93356e8fff62951b56e7d27a7d2282f9faf3
guilherme <guilhermeleobas@gmail.com>
2018-07-16 15:11:47 -0300

    Remove benchmarks

--- Benchs/Prolangs-C/fixoutput/info.sh
+++
@@ -1,7 +0,0 @@
-bnc_name="$(basename $(pwd))" ;
-lnk_name="$bnc_name.rbc" ;
-prf_name="$bnc_name.ibc" ;
-obj_name="$bnc_name.o" ;
-exe_name="$bnc_name.exe" ;
-
-source_files=( "fixoutput.c" "stringI.c" )
