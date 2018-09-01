bea695f49c0b06d63972868187493b8543490774
Guilherme <guilhermeleobas@gmail.com>
2018-09-01 14:20:36 -0300

    update scripts

--- exec.sh
+++ exec.sh
@@ -17,7 +17,8 @@ function execute() {
   echo "$cmd"
   
   if [[ -n $SANITIZE && $SANITIZE -eq 1 ]]; then
-    echo "cd $(pwd) && ASAN_OPTIONS=halt_on_error=0 $cmd" >> $BASEDIR/run.txt ;
+    # echo "cd $(pwd) && SANITIZER_STATS_PATH=a.stats ASAN_OPTIONS=halt_on_error=0 $cmd" >> $BASEDIR/run.txt ;
+    echo "cd $(pwd) && ASAN_SYMBOLIZER_PATH=$HOME/Programs/llvm61/build/bin/llvm-symbolizer ASAN_OPTIONS=halt_on_error=false:print_stats=true $cmd" >> $BASEDIR/run.txt ;
   else
     echo "cd $(pwd) && $cmd" >> $BASEDIR/run.txt ;
   fi
--- parallel.sh
+++ parallel.sh
@@ -2,5 +2,5 @@
 
 if [[ $EXEC -eq 1 ]]; then
   echo 'STARTING EXECUTION' ;
-  parallel --verbose --joblog run.log  --jobs $JOBS < run.txt ;
+  parallel --tty --verbose --joblog run.log  --jobs $JOBS < run.txt ;
 fi
