9427ce6dce14cace5dd9ca8fb11f11deab37e06f
Guilherme Leobas <guilhermeleobas@gmail.com>
2018-09-21 15:52:10 +0200

    update instrument.sh

--- instrument.sh
+++ instrument.sh
@@ -16,14 +16,16 @@ function compile() {
   fi
 
   # source_files is the variable with all the files we're gonna compile
-  parallel --tty --jobs=${JOBS} $LLVM_PATH/$COMPILER $CXXFLAGS -Xclang -disable-O0-optnone -g -S -c -emit-llvm {} -o {.}.bc ::: "${source_files[@]}" ;
-  parallel --tty --jobs=${JOBS} $LLVM_PATH/opt -mem2reg -S -load $PHOENIX_PATH/build/Count/Count.$suffix -Count {.}.bc -o {.}.rbc ::: "${source_files[@]}" ;
+  parallel --tty --jobs=${JOBS} $LLVM_PATH/$COMPILER $CXXFLAGS -Xclang -disable-O0-optnone -S -c -emit-llvm {} -o {.}.bc ::: "${source_files[@]}" ;
+  # -debug-only=Count
+  # parallel --tty --jobs=${JOBS} $LLVM_PATH/opt -mem2reg -S -load $PHOENIX_PATH/build/Count/Count.$suffix -Count {.}.bc -o {.}.rbc ::: "${source_files[@]}" ;
+  parallel --tty --jobs=${JOBS} $LLVM_PATH/opt -mem2reg -S {.}.bc -o {.}.rbc ::: "${source_files[@]}" ;
   
   #Generate all the bcs into a big bc:
   $LLVM_PATH/llvm-link -S *.rbc -o $lnk_name ;
 
   # Insert instrumentation in the program:
-  $LLVM_PATH/opt $lnk_name -o $prf_name ;
+  $LLVM_PATH/opt -load $PHOENIX_PATH/build/Count/Count.$suffix -Count $lnk_name -o $prf_name ;
     
   # Compile our instrumented file, in IR format, to x86:
   $LLVM_PATH/llc -filetype=obj $prf_name -o $obj_name ;
