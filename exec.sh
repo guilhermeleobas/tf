b209ea26151fa4f8a6ee47190fc2fe3586cec63e
Guilherme Leobas <guilhermeleobas@gmail.com>
2019-03-11 14:58:50 -0300

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -2,6 +2,16 @@
 
 function execute() {
   
+  exe=$exe_name ;
+
+  if [[ -n $INSTRUMENT && $INSTRUMENT -eq 1 ]]; then
+    exe=INS_$exe_name
+  fi
+  
+  if [[ -n $SANITIZE && $SANITIZE -eq 1 ]]; then
+    exe=SAN_$exe_name
+  fi
+  
   if [[ $(pwd) =~ "cBench" ]]; then
     for i in $(seq 1 $CBENCH); do
       cmd="./__run $i $exe_name"
@@ -11,17 +21,18 @@ function execute() {
   fi
 
   if [[ $DIFF -eq 1 ]]; then
-    cmd="$TIMEOUT --signal=TERM ${RUNTIME} ./$exe_name $RUN_OPTIONS < $STDIN &> $bench_name.output && \
+    cmd="$TIMEOUT --signal=TERM ${RUNTIME} ./$exe $RUN_OPTIONS < $STDIN &> $bench_name.output && \
          $BASEDIR/DiffOutput.sh $bench_name $bench_name.reference_output $bench_name.output"
+  elif [[ $HYPERFINE -eq 1 ]]; then
+    cmd="hyperfine \"./$exe $RUN_OPTIONS < $STDIN > $STDOUT 2> $STDOUT \" -w 1 -r 4 --show-output --export-csv time_$exe.csv -u second  "
   else
-    cmd="$TIMEOUT --signal=TERM ${RUNTIME} ./$exe_name $RUN_OPTIONS < $STDIN > $STDOUT"
+    cmd="$TIMEOUT --signal=TERM ${RUNTIME} ./$exe $RUN_OPTIONS < $STDIN &> $STDOUT"
   fi
 
   echo "$cmd"
   
-  if [[ -n $ASAN && $ASAN -eq 1 ]]; then
-    echo "cd $(pwd) && ASAN_SYMBOLIZER_PATH=$HOME/Programs/llvm61/build/bin/llvm-symbolizer \
-      ASAN_OPTIONS=halt_on_error=false:print_stats=true $cmd" >> /tmp/run.txt
+  if [[ -n $SANITIZE && $SANITIZE -eq 1 ]]; then
+    echo "cd $(pwd) && ASAN_SYMBOLIZER_PATH=$HOME/Programs/llvm61/build/bin/llvm-symbolizer ASAN_OPTIONS=halt_on_error=false:print_stats=true $cmd" >> $BASEDIR/run.txt
   else
     echo "cd $(pwd) && $cmd" >> /tmp/run.txt
   fi
