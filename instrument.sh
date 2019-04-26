f056473505878bdd0343cdbf0ad43d0d6c65d66c
Guilherme Leobas <guilhermeleobas@gmail.com>
2019-04-26 17:26:38 -0300

    Update instrument.sh

--- instrument.sh
+++ instrument.sh
@@ -26,10 +26,10 @@ function compile() {
   
   # common optimizations
   $LLVM_PATH/opt -S -mem2reg -instcombine -early-cse -instnamer $lnk_name -o $prf_name.opt.1
-  # my optimization
-  $LLVM_PATH/opt -S -load $pass_path -$PASS $prf_name.opt.1 -o $prf_name.opt.2
   # Opt
-  $LLVM_PATH/opt -S ${OPT} $prf_name.opt.2 -o $prf_name.opt.3
+  $LLVM_PATH/opt -S ${OPT} $prf_name.opt.1 -o $prf_name.opt.2
+  # my optimization
+  $LLVM_PATH/opt -S -load $pass_path -$PASS -O3 $prf_name.opt.2 -o $prf_name.opt.3
   
   if [[ $PASS = "CountArith" ]]; then
     # Compile our instrumented file, in IR format, to x86:
