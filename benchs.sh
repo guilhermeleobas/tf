3bb0c1a681bcd3f70c0c91f9235e21bc916f55ab
Guilherme Leobas <guilhermeleobas@gmail.com>
2019-06-19 02:30:32 -0300

    upd benchs.sh

--- benchs.sh
+++ benchs.sh
@@ -41,34 +41,49 @@ function mediabench() {
 }
 
 function PolyBench(){
-  dirs=("linear-algebra/kernels/2mm" "linear-algebra/kernels/3mm" "linear-algebra/kernels/atax"
-        "linear-algebra/kernels/bicg" "linear-algebra/kernels/doitgen" "linear-algebra/kernels/mvt" 
-        "linear-algebra/solvers/cholesky" "linear-algebra/solvers/durbin" "linear-algebra/solvers/gramschmidt"
-        "linear-algebra/solvers/lu" "linear-algebra/solvers/ludcmp" "linear-algebra/solvers/trisolv"
-        "linear-algebra/blas/gemm" "linear-algebra/blas/gemver" "linear-algebra/blas/gesummv"
-        "linear-algebra/blas/symm" "linear-algebra/blas/syr2k" "linear-algebra/blas/syrk"
-        "linear-algebra/blas/trmm"
-        "datamining/correlation" "datamining/covariance"
-        "medley/floyd-warshall" "medley/deriche" "medley/nussinov"
-        "stencils/adi" "stencils/fdtd-2d" "stencils/heat-3d" "stencils/jacobi-1d" "stencils/jacobi-2d"
-        "stencils/seidel-2d")
+  # dirs=("linear-algebra/kernels/2mm" "linear-algebra/kernels/3mm" "linear-algebra/kernels/atax"
+  #       "linear-algebra/kernels/bicg" "linear-algebra/kernels/doitgen" "linear-algebra/kernels/mvt" 
+  #       "linear-algebra/solvers/cholesky" "linear-algebra/solvers/durbin" "linear-algebra/solvers/gramschmidt"
+  #       "linear-algebra/solvers/lu" "linear-algebra/solvers/ludcmp" "linear-algebra/solvers/trisolv"
+  #       "linear-algebra/blas/gemm" "linear-algebra/blas/gemver" "linear-algebra/blas/gesummv"
+  #       "linear-algebra/blas/symm" "linear-algebra/blas/syr2k" "linear-algebra/blas/syrk"
+  #       "linear-algebra/blas/trmm"
+  #       "datamining/correlation" "datamining/covariance"
+  #       "medley/floyd-warshall" "medley/deriche" "medley/nussinov"
+  #       "stencils/adi" "stencils/fdtd-2d" "stencils/heat-3d" "stencils/jacobi-1d" "stencils/jacobi-2d"
+  #       "stencils/seidel-2d")
+
+  dirs=("datamining/correlation" "datamining/covariance" "linear-algebra/blas/gemm" 
+      "linear-algebra/blas/gemver" "linear-algebra/blas/gesummv" "linear-algebra/blas/symm" 
+      "linear-algebra/blas/syrk" "linear-algebra/blas/trmm" "linear-algebra/kernels/2mm" 
+      "linear-algebra/kernels/3mm" "linear-algebra/kernels/atax" "linear-algebra/kernels/bicg" 
+      "linear-algebra/kernels/doitgen" "linear-algebra/kernels/mvt" 
+      "linear-algebra/solvers/cholesky" "linear-algebra/solvers/gramschmidt"
+      "linear-algebra/solvers/lu" "linear-algebra/solvers/ludcmp"
+      "linear-algebra/solvers/trisolv" "stencils/fdtd-2d")
+  
   walk "${dirs[@]}"
 }
 
 function cBench(){
-  dirs=("security_rijndael_d/src" "automotive_susan_c/src"
-        "security_rijndael_e/src" "consumer_jpeg_c/src" "consumer_lame/src"
-        "bzip2e/src" "telecom_adpcm_d/src" "bzip2d/src"
-        "network_dijkstra/src" "office_stringsearch1/src"
-        "consumer_tiffdither/src" "automotive_qsort1/src" 
-        "consumer_jpeg_d/src" "automotive_susan_e/src" 
-        "automotive_bitcount/src" "security_blowfish_e/src" "consumer_tiff2bw/src"
-        "telecom_CRC32/src" "security_sha/src" "consumer_tiffmedian/src"
-        "automotive_susan_s/src" "network_patricia/src" "telecom_adpcm_c/src"
-        "consumer_tiff2rgba/src" "security_blowfish_d/src")
+  # dirs=("security_rijndael_d/src" "automotive_susan_c/src"
+  #       "security_rijndael_e/src" "consumer_jpeg_c/src" "consumer_lame/src"
+  #       "bzip2e/src" "telecom_adpcm_d/src" "bzip2d/src"
+  #       "network_dijkstra/src" "office_stringsearch1/src"
+  #       "consumer_tiffdither/src" "automotive_qsort1/src" 
+  #       "consumer_jpeg_d/src" "automotive_susan_e/src" 
+  #       "automotive_bitcount/src" "security_blowfish_e/src" "consumer_tiff2bw/src"
+  #       "telecom_CRC32/src" "security_sha/src" "consumer_tiffmedian/src"
+  #       "automotive_susan_s/src" "telecom_adpcm_c/src"
+  #       "consumer_tiff2rgba/src" "security_blowfish_d/src")
       
-  cant=("consumer_mad/src" "office_ispell/src" "office_ghostscript/src" "office_rsynth/src"
-  "security_pgp_d/src" "security_pgp_e/src" "telecom_gsm/src")
+  # cant=("consumer_mad/src" "office_ispell/src" "office_ghostscript/src" "office_rsynth/src"
+  # "security_pgp_d/src" "security_pgp_e/src" "telecom_gsm/src" "network_patricia/src")
+  
+  dirs=("security_blowfish_d/src" "consumer_tiff2rgba/src" "consumer_tiffmedian/src" 
+      "consumer_tiff2bw/src" "security_blowfish_e/src" "consumer_jpeg_d/src" 
+      "consumer_tiffdither/src" "bzip2d/src" "bzip2e/src" "consumer_lame/src" 
+      "consumer_jpeg_c/src" "security_rijndael_e/src" "security_rijndael_d/src")
   
   walk "${dirs[@]}"
 }
@@ -88,4 +103,4 @@ benchs=( "ASC_Sequoia" "BenchmarkGame" "BitBench" "CoyoteBench" "DOE_ProxyApps_C
   "Trimaran" "TSVC" "PAQ8p" "NPB-serial" "VersaBench" "FreeBench" "cBench"
   "MallocBench" "McCat" "Olden" "Prolangs-C" "cpu2006")
   
-benchs=("PolyBench")
+benchs=("cBench")
