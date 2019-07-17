323220f9e64e8c9850672767006e1fcfad33b74a
Guilherme Leobas <guilhermeleobas@gmail.com>
2019-07-17 19:33:50 -0300

    Update instrument.sh

--- instrument.sh
+++ instrument.sh
@@ -2,50 +2,28 @@
 
 # this is left as an example 
 function compile() {
-
-	pass_path=""
-
-	[[ -n $pass_path ]] || {
-		echo "One must define the pass path when INSTRUMENT=1"
-		exit 1
-	}
-
-	[[ -n $PASS ]] || {
-		echo "One must define PASS when INSTRUMENT=1"
-		exit 1
-	}
-
-  if [[ -n $CPU2006 && $CPU2006 -eq 1 ]]; then
-    # Convert the program to SSA form:
-    $LLVM_PATH/opt -mem2reg -load $pass_path -$PASS -S $rbc_name -o $prf_name
-
-    # Compile our file, in IR format, to x86:
-    $LLVM_PATH/llc -filetype=obj $prf_name -o $obj_name
-    # Compile everything now, producing a final executable file:
-    $LLVM_PATH/$COMPILER -lm $obj_name -o $exe_name
-    
-    return
-  fi
-
-  # source_files is the variable with all the files we're gonna compile
-  parallel --tty --jobs=${JOBS} $LLVM_PATH/$COMPILER $COMPILE_FLAGS \
-    -Xclang -disable-O0-optnone \
-    -S -c -emit-llvm {} -o {.}.bc ::: "${source_files[@]}" 
   
-  parallel --tty --jobs=${JOBS} $LLVM_PATH/opt -S {.}.bc -o {.}.rbc ::: "${source_files[@]}"
-
-  #Generate all the bcs into a big bc:
-  $LLVM_PATH/llvm-link -S *.rbc -o $lnk_name
-
-  # Optmize 
-  if [[ $SSA -eq 1 ]]; then
-    $LLVM_PATH/opt -S -mem2reg -load $pass_path -$PASS -verify $lnk_name -o $prf_name
+  if [[ -n $CPU2006 && $CPU2006 -eq 1 ]]; then
+    # rbc -> lnk
+    $LLVM_PATH/opt -S $rbc_name -o $lnk_name
   else
-    $LLVM_PATH/opt -S -load $pass_path -$PASS -verify $lnk_name -o $prf_name
+    # source_files is the variable with all the files we're gonna compile
+    parallel --tty --jobs=${JOBS} $LLVM_PATH/$COMPILER $COMPILE_FLAGS \
+      -Xclang -disable-O0-optnone \
+      -S -c -emit-llvm {} -o {.}.bc ::: "${source_files[@]}" 
+    
+    parallel --tty --jobs=${JOBS} $LLVM_PATH/opt -S {.}.bc -o {.}.rbc ::: "${source_files[@]}"
+  
+    #Generate all the bcs into a big bc:
+    $LLVM_PATH/llvm-link -S *.rbc -o $lnk_name
   fi
-
-  # Compile our instrumented file, in IR format, to x86:
-  $LLVM_PATH/llc -filetype=obj $prf_name -o $obj_name
+  
+  # Optimizations
+  $LLVM_PATH/opt -S -load $PASS_PATH -${PASS} $lnk_name -o $prf_name
+  
+  # Generate obj file
+  $LLVM_PATH/llc -filetype=obj $prf_name -o $obj_name ;
+  
   # Compile everything now, producing a final executable file:
-  $LLVM_PATH/$COMPILER -lm $obj_name -o $exe_name
+  $LLVM_PATH/$COMPILER -lm $obj_name -o INS_$exe_name ;
 }
